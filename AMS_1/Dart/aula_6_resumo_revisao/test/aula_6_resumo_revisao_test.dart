272611b2d07be01076147c306bc8ac079a1e0fa3
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2026-08-06 16:04:08 -0300

    Add files via upload

---
+++ AMS_1/Dart/aula_6_resumo_revisao/test/aula_6_resumo_revisao_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_6_resumo_revisao/aula_6_resumo_revisao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ AMS_1/Dart/aula_9_atv_player/test/aula_9_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_9/aula_9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
