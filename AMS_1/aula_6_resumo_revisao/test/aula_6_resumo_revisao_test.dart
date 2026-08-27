16e6aab29c61999e323c057d24c08e6a340a5ac2
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2026-08-27 17:20:40 -0300

    Delete AMS_1/aula_6_resumo_revisao directory

--- AMS_1/aula_6_resumo_revisao/test/aula_6_resumo_revisao_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:aula_6_resumo_revisao/aula_6_resumo_revisao.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
